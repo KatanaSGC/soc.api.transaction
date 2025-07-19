cb99163922b3f9740475f2217858d40fa7bc2c0f
Agustin Gonzalez <agm.sge@outlook.com>
2025-07-18 21:06:44 -0600

    add hello view

--- src/controller/aliveSignal.controller.ts
+++ src/controller/aliveSignal.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Render } from '@nestjs/common';
 
 @Controller('transactions/alive-signal')
 export class AliveSignalController {
@@ -6,5 +6,23 @@ export class AliveSignalController {
   @Get('/status')
   getAliveSignal(): { status: string } {
     return { status: 'Service is alive' };
+  }
+
+  @Get()
+  @Render('index')
+  getHello() {
+    return {
+      name: 'Juan',
+      date: new Date().toLocaleDateString('es-ES')
+    };
+  }
+
+  @Get('/hello/:name')
+  @Render('index')
+  getHelloWithParam(@Param('name') name: string) {
+    return {
+      name: name,
+      date: new Date().toLocaleDateString('es-ES')
+    };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { GlobalExceptionFilter } from './filter/globalException.filter';
 import { ResponseInterceptor } from './interceptor/response.interceptor';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
 
   app.useGlobalPipes(
     new ValidationPipe({
